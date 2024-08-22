20cf76d64dda658316f85c841ced1c1afd082a22
Armando Valdez <155396846+valde148@users.noreply.github.com>
2024-08-22 14:02:51 -0500

    command

---
+++ Lab1.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class Lab1 {
+
+    public static void main(String[]args){
+        int i;
+        int n [] = new int[3];
+        Scanner scnr = new Scanner(System.in);
+        for (i = 0; i < 3 ; i++){
+            n[i] = scnr.nextInt();
+            
+        }
+            
+        }
+    }
+    
